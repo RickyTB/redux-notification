4239d57dbbbcbceae0f768a59d31ba601c838467
Ricardo Baquero <wola_ricky@hotmail.com>
2019-10-09 21:10:02 -0500

    Updates

---
+++ precache-manifest.8327c732bd5bea1539e38e06e1ce9450.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a858b6c1988d37260f05445a685d47ea",
+    "url": "/redux-notification/index.html"
+  },
+  {
+    "revision": "7d139d964dee8b0faa8b",
+    "url": "/redux-notification/static/css/2.ff29e5f6.chunk.css"
+  },
+  {
+    "revision": "7d139d964dee8b0faa8b",
+    "url": "/redux-notification/static/js/2.a89f718d.chunk.js"
+  },
+  {
+    "revision": "5baa535da2fb397bfec7",
+    "url": "/redux-notification/static/js/main.8ef3ad39.chunk.js"
+  },
+  {
+    "revision": "9330f54a53b75df19662",
+    "url": "/redux-notification/static/js/runtime~main.633478ba.js"
+  }
+]);
